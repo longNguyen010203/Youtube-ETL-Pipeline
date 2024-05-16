86ef01866ebfa71ddbe68197b822315b2ef07634
Long Nguyen <zedsc03@gmail.com>
2024-05-16 20:37:54 +0700

    Update MySQL Schema and Load

--- load_dataset/mysql_load.sql
+++ load_dataset/mysql_load.sql
@@ -1,75 +1,33 @@
 LOAD DATA LOCAL INFILE '/tmp/youTube_trending_video/DE_youtube_trending_data.csv' 
-INTO TABLE youtube_trending_data 
+INTO TABLE DE_youtube_trending_data 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 LOAD DATA LOCAL INFILE '/tmp/youTube_trending_video/JP_youtube_trending_data.csv' 
-INTO TABLE youtube_trending_data 
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n' 
-IGNORE 1 ROWS;
-
-LOAD DATA LOCAL INFILE '/tmp/youTube_trending_video/KR_youtube_trending_data.csv'
-INTO TABLE youtube_trending_data 
+INTO TABLE JP_youtube_trending_data 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n' 
 IGNORE 1 ROWS;
 
 LOAD DATA LOCAL INFILE '/tmp/youTube_trending_video/RU_youtube_trending_data.csv' 
-INTO TABLE youtube_trending_data 
+INTO TABLE RU_youtube_trending_data 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/tmp/youTube_trending_video/US_youtube_trending_data.csv' 
-INTO TABLE youtube_trending_data 
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n' 
-IGNORE 1 ROWS;
-
-LOAD DATA LOCAL INFILE '/tmp/youTube_trending_video/BR_youtube_trending_data.csv' 
-INTO TABLE youtube_trending_data 
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n' 
-IGNORE 1 ROWS;
-
 LOAD DATA LOCAL INFILE '/tmp/youTube_trending_video/CA_youtube_trending_data.csv' 
-INTO TABLE youtube_trending_data 
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n' 
-IGNORE 1 ROWS;
-
-LOAD DATA LOCAL INFILE '/tmp/youTube_trending_video/FR_youtube_trending_data.csv' 
-INTO TABLE youtube_trending_data 
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n' 
-IGNORE 1 ROWS;
-
-LOAD DATA LOCAL INFILE '/tmp/youTube_trending_video/GB_youtube_trending_data.csv' 
-INTO TABLE youtube_trending_data 
+INTO TABLE CA_youtube_trending_data 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n' 
 IGNORE 1 ROWS;
 
 LOAD DATA LOCAL INFILE '/tmp/youTube_trending_video/IN_youtube_trending_data.csv' 
-INTO TABLE youtube_trending_data 
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n' 
-IGNORE 1 ROWS;
-
-LOAD DATA LOCAL INFILE '/tmp/youTube_trending_video/MX_youtube_trending_data.csv' 
-INTO TABLE youtube_trending_data 
+INTO TABLE IN_youtube_trending_data 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n' 
--- load_dataset/mysql_schemas.sql
+++ load_dataset/mysql_schemas.sql
@@ -1,5 +1,8 @@
 DROP TABLE IF EXISTS youtube_trending_data;
-CREATE TABLE youtube_trending_data (
+
+
+DROP TABLE IF EXISTS CA_youtube_trending_data;
+CREATE TABLE CA_youtube_trending_data (
     video_id VARCHAR(20),
     title TEXT,
     publishedAt VARCHAR(27),
@@ -14,5 +17,86 @@ CREATE TABLE youtube_trending_data (
     comment_count TEXT,
     thumbnail_link TEXT,
     comments_disabled VARCHAR(6),  
-    ratings_disabled VARCHAR(6)
+    ratings_disabled VARCHAR(6),
+    description TEXT
+);
+
+DROP TABLE IF EXISTS DE_youtube_trending_data;
+CREATE TABLE DE_youtube_trending_data (
+    video_id VARCHAR(20),
+    title TEXT,
+    publishedAt VARCHAR(27),
+    channelId VARCHAR(27),
+    channelTitle TEXT,
+    categoryId VARCHAR(5),
+    trending_date VARCHAR(27),
+    tags TEXT,
+    view_count TEXT,
+    likes TEXT,
+    dislikes TEXT,
+    comment_count TEXT,
+    thumbnail_link TEXT,
+    comments_disabled VARCHAR(6),  
+    ratings_disabled VARCHAR(6),
+    description TEXT
+);
+
+DROP TABLE IF EXISTS IN_youtube_trending_data;
+CREATE TABLE IN_youtube_trending_data (
+    video_id VARCHAR(20),
+    title TEXT,
+    publishedAt VARCHAR(27),
+    channelId VARCHAR(27),
+    channelTitle TEXT,
+    categoryId VARCHAR(5),
+    trending_date VARCHAR(27),
+    tags TEXT,
+    view_count TEXT,
+    likes TEXT,
+    dislikes TEXT,
+    comment_count TEXT,
+    thumbnail_link TEXT,
+    comments_disabled VARCHAR(6),  
+    ratings_disabled VARCHAR(6),
+    description TEXT
+);
+
+DROP TABLE IF EXISTS JP_youtube_trending_data;
+CREATE TABLE JP_youtube_trending_data (
+    video_id VARCHAR(20),
+    title TEXT,
+    publishedAt VARCHAR(27),
+    channelId VARCHAR(27),
+    channelTitle TEXT,
+    categoryId VARCHAR(5),
+    trending_date VARCHAR(27),
+    tags TEXT,
+    view_count TEXT,
+    likes TEXT,
+    dislikes TEXT,
+    comment_count TEXT,
+    thumbnail_link TEXT,
+    comments_disabled VARCHAR(6),  
+    ratings_disabled VARCHAR(6),
+    description TEXT
+);
+
+DROP TABLE IF EXISTS RU_youtube_trending_data;
+CREATE TABLE RU_youtube_trending_data (
+    video_id VARCHAR(20),
+    title TEXT,
+    publishedAt VARCHAR(27),
+    channelId VARCHAR(27),
+    channelTitle TEXT,
+    categoryId VARCHAR(5),
+    trending_date VARCHAR(27),
+    tags TEXT,
+    view_count TEXT,
+    likes TEXT,
+    dislikes TEXT,
+    comment_count TEXT,
+    thumbnail_link TEXT,
+    comments_disabled VARCHAR(6),  
+    ratings_disabled VARCHAR(6),
+    description TEXT
 );
